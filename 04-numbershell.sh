a87fd1a58547895acc50abc01cf9b1f3c14e6e30
.email.com <gangisettymani98@gmail.com>
2026-06-12 20:54:17 +0530

    i have edit the file

---
+++ 04-numbershell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+number = 10
+number = 20
+
+if [$number - gt 10];then
+echo "number is less than 20 is true"
+elif 
+echo "number is greater than 20 is true"
+else
+echo "number is less than is 10 flase"
+fi
+
