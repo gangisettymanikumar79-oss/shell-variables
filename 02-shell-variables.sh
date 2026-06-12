c09e82dac97e6d949e64217d952ca071143f4044
.email.com <gangisettymani98@gmail.com>
2026-06-12 19:11:22 +0530

    shell-variable file

--- 02-shell-variables.sh
+++ 02-shell-variables.sh
@@ -10,7 +10,7 @@ echo "who is running this : $user"
 echo "which directory : $pwd"
 echo "Home directory : $Home"
 echo "PID of the current script :$$"
-sleep5&
+sleep5
 echo "pid of the background command running just now : $!"
 
 
