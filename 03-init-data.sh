4576d52ca6ee2ead8bcd4fac1f3d959cb8b4dd40
.email.com <gangisettymani98@gmail.com>
2026-06-12 20:27:33 +0530

    i have edit the file

--- 03-init-data.sh
+++ 03-init-data.sh
@@ -3,3 +3,10 @@ num1=10
 num2=30
 sum=$((num1+num2))
 echo "sum is : $sum"
+
+#array
+Movies=("RRR"  "varanasi"  "policeman")
+echo "movie Area:${movies[@]}"
+echo "movie Area:${movies[1]}"
+echo "movie Area:${movies[2]}"
+
