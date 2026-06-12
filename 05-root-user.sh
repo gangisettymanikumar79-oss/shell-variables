996920cfb0de35c0f12fb5caa60a58efc51726d9
.email.com <gangisettymani98@gmail.com>
2026-06-12 21:27:19 +0530

    i have edit the file

--- 05-root-user.sh
+++ 05-root-user.sh
@@ -6,8 +6,13 @@ if [$user -ne 0]; then
  echo "the root access the script with proper reason"
  exit
 fi 
- echo "i am continuing......."
- 
+ # echo "i am continuing......."
+echo "installing mysql"
+dnf install mysql -y
+
+echo "i am continuing......."
+echo "i am still continuing......."
+
 
 
 
