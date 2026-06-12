e652edf4d175f5d0f357c51612075b7aac43c684
.email.com <gangisettymani98@gmail.com>
2026-06-12 21:54:04 +0530

    i have edit the file

--- 05-root-user.sh
+++ 05-root-user.sh
@@ -2,19 +2,24 @@
 
 user=(id -u)
 # check the root access or not
-if [$user -ne 0]; then
+if [ $user -ne 0 ]; then
    echo "the root access the script with proper reason"
    exit
 fi 
  # echo "i am continuing......."
-   echo "installing mysql"
-   dnf install mysql -y
-
-if [$? -ne 0]; then
-   echo "installing mysql...........FAILD"
-   exit
+   dnf list installing mysql
+if [ $? -eq 0 ]; then
+   echo "installing mysql...........skipping"
 else
-   echo "installing mysql.............success"
+    echo "installing mysql"
+    dnf install mysql -y
+
+   if [ $? -ne 0 ]; then
+      echo "installing mysql...........FAILD"
+      exit 1
+  else
+       echo "installing mysql.............success"
+  fi
 fi
 
 
