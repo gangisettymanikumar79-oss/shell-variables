241f566892a68b53b6dd6e65e9831808ac98560d
.email.com <gangisettymani98@gmail.com>
2026-06-13 21:40:04 +0530

    i want create by the new file

--- 09-backup.sh
+++ 09-backup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-for i in 1 2 3 4 5 6 7 8 9
-do
-    echo "Iternation : $i"
-done
+for i in {1..5}
+    do 
+        echo "iteration :$i"
+    done
+    count=1
+while [ $count -le 5 ] 
+    do
+        echo "count:$count"
+        count=$((count ++))
+    done
 
-
-
-    #count=1
-    #while [ $count -le 5 ]
-    #do 
-    #echo "count : $count"
-    #count=$(($count + 1))
-    #done     
